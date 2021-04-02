e38875103b6e666e0415a81d382b0534d0aa7d77
NoxuuLab <77098884+NoxuuLab@users.noreply.github.com>
2021-04-02 14:55:04 +0200

    import libraries and construct argument parser
    added bw2color_image py file
    
    add historical images in the project

---
+++ bw2color_image.py
@@ -0,0 +1,16 @@
+# import libraries and packages
+import numpy as np
+import argparse
+import cv2
+
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", type=str, required=True,
+	help="path to input black and white image")
+ap.add_argument("-p", "--prototxt", type=str, required=True,
+	help="path to Caffe prototxt file")
+ap.add_argument("-m", "--model", type=str, required=True,
+	help="path to Caffe pre-trained model")
+ap.add_argument("-c", "--points", type=str, required=True,
+	help="path to cluster center points")
+args = vars(ap.parse_args())
