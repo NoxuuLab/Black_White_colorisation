cd16145841d437b01957fee5ed4ad1cc737a1067
NoxuuLab <77098884+NoxuuLab@users.noreply.github.com>
2021-04-02 15:07:55 +0200

    install opencv2 for import
    import libraries and construct argument parser
    
    added bw2color_image py file
    
    add historical images in the project

--- bw2color_image.py
+++ bw2color_image.py
@@ -6,11 +6,11 @@ import cv2
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", type=str, required=True,
-	help="path to input black and white image")
+                help="path to input black and white image")
 ap.add_argument("-p", "--prototxt", type=str, required=True,
-	help="path to Caffe prototxt file")
+                help="path to Caffe prototxt file")
 ap.add_argument("-m", "--model", type=str, required=True,
-	help="path to Caffe pre-trained model")
+                help="path to Caffe pre-trained model")
 ap.add_argument("-c", "--points", type=str, required=True,
-	help="path to cluster center points")
+                help="path to cluster center points")
 args = vars(ap.parse_args())
